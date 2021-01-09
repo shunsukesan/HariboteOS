279a41bac1ed874304c13767d36ecf267a3f160e
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-09 16:48:37 +0900

    fix

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,15 +1,14 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
 	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
-    char *p;
+	char *p; /* pという変数は、BYTE [...]用の番地 */
 
-    p = (char *) 0xa0000;
+	p = (char *) 0xa0000;
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-        *(p+1) = i & 0x0f;
+	for (i = 0; i <= 0xffff; i++) {
+		*(p + i) = i & 0x0f;
 	}
 
 	for (;;) {
