78c5a4c5690a23869564cde8c05352348c953d59
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-10 13:40:05 +0900

    activate keyboard mouse int

--- src/bootpack.c
+++ src/bootpack.c
@@ -17,6 +17,9 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
+	io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+
 	for (;;) {
 		io_hlt();
 	}
