dfe70f924aaa26f0002d3c862399dfc6e9de52ef
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-11 13:14:10 +0900

    memory management

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,27 +1,47 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+#define MEMMAN_FREES		4090	/* これで約32KB */
+#define MEMMAN_ADDR			0x003c0000
+
+struct FREEINFO {
+	unsigned int addr, size;
+};
+
+struct MEMMAN {
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+
 unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
+	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
 	init_gdtidt();
 	init_pic();
 	io_sti(); /* IDT/PICの初期化が終わったのでCPUの割り込み禁止を解除 */
-
 	fifo8_init(&keyfifo, 32, keybuf);
 	fifo8_init(&mousefifo, 128, mousebuf);
-
 	io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
 	io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
 
 	init_keyboard();
 	enable_mouse(&mdec);
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	memman_init(memman);
+	memman_free(memman, 0x00001000, 0x0009e000);
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -117,4 +137,106 @@ unsigned int memtest(unsigned int start, unsigned int end)
 	}
 
 	return i;
+}
+
+void memman_init(struct MEMMAN *man)
+{
+	man->frees = 0;
+	man->maxfrees = 0;
+	man->lostsize = 0;
+	man->losts = 0;
+	return;
+}
+
+unsigned int memman_total(struct MEMMAN *man)
+{
+	unsigned int i, t = 0;
+	for (i = 0; i < man->frees; i++) {
+		t += man->free[i].size;
+	}
+	return t;
+}
+
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+{
+	unsigned int i, a;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].size >= size) {
+			// 十分な空きを発見
+			a = man->free[i].addr;
+			man->free[i].addr += size;
+			man->free[i].size -= size;
+			if (man->free[i].size = 0) {
+				man->frees--;
+				for (; i < man->frees; i++) {
+					man->free[i] = man->free[i+1];
+				}
+			}
+			return a;
+		}
+	}
+	return 0;
+}
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
+/* 解放 */
+{
+	int i, j;
+	/* まとめやすさを考えると、free[]がaddr順に並んでいるほうがいい */
+	/* だからまず、どこに入れるべきかを決める */
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].addr > addr) {
+			break;
+		}
+	}
+	/* free[i - 1].addr < addr < free[i].addr */
+	if (i > 0) {
+		/* 前がある */
+		if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
+			/* 前のあき領域にまとめられる */
+			man->free[i - 1].size += size;
+			if (i < man->frees) {
+				/* 後ろもある */
+				if (addr + size == man->free[i].addr) {
+					/* なんと後ろともまとめられる */
+					man->free[i - 1].size += man->free[i].size;
+					/* man->free[i]の削除 */
+					/* free[i]がなくなったので前へつめる */
+					man->frees--;
+					for (; i < man->frees; i++) {
+						man->free[i] = man->free[i + 1]; /* 構造体の代入 */
+					}
+				}
+			}
+			return 0; /* 成功終了 */
+		}
+	}
+	/* 前とはまとめられなかった */
+	if (i < man->frees) {
+		/* 後ろがある */
+		if (addr + size == man->free[i].addr) {
+			/* 後ろとはまとめられる */
+			man->free[i].addr = addr;
+			man->free[i].size += size;
+			return 0; /* 成功終了 */
+		}
+	}
+	/* 前にも後ろにもまとめられない */
+	if (man->frees < MEMMAN_FREES) {
+		/* free[i]より後ろを、後ろへずらして、すきまを作る */
+		for (j = man->frees; j > i; j--) {
+			man->free[j] = man->free[j - 1];
+		}
+		man->frees++;
+		if (man->maxfrees < man->frees) {
+			man->maxfrees = man->frees; /* 最大値を更新 */
+		}
+		man->free[i].addr = addr;
+		man->free[i].size = size;
+		return 0; /* 成功終了 */
+	}
+	/* 後ろにずらせなかった */
+	man->losts++;
+	man->lostsize += size;
+	return -1; /* 失敗終了 */
 }
