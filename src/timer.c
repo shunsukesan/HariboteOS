125189e94d61247ae6b7697bb242eb2fd1871954
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-16 13:33:52 +0900

    counter

--- src/timer.c
+++ src/timer.c
@@ -8,12 +8,14 @@ void init_pit(void)
     io_out8(PIT_CTRL, 0x34);
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
+    timerctl.count = 0;
     return;
 }
 
 void inthandler20(int *esp)
 {
 	io_out8(PIC0_OCW2, 0x60);	/* IRQ-00受付完了をPICに通知 */
-	/* とりあえずなにもしない */
+    timerctl.count++;
 	return;
 }
+
