9f4ea880476b8794f52e8f6465afb27560ee6f26
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-17 16:38:38 +0900

    up

--- src/bootpack.c
+++ src/bootpack.c
@@ -12,7 +12,7 @@ struct TSS32 {
 	int ldtr, iomap;
 };
 
-void task_b_main(void);
+void task_b_main(struct SHEET *sht_back);
 
 void HariMain(void)
 {
@@ -121,7 +121,7 @@ void HariMain(void)
 	tss_b.ds = 1 * 8;
 	tss_b.fs = 1 * 8;
 	tss_b.gs = 1 * 8;
-	*((int *) 0x0fec) = (int) sht_back;
+	*((int *) (task_b_esp + 4)) = (int) sht_back;
 	
 	for (;;) {
 		io_cli();
@@ -207,8 +207,6 @@ void HariMain(void)
 			}
 		}
 	}
-
-	return;
 }
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title)
@@ -283,31 +281,34 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c)
 	return;
 }
 
-void task_b_main(void)
+void task_b_main(struct SHEET *sht_back)
 {
 	struct FIFO32 fifo;
-	struct TIMER *timer_ts;
+	struct TIMER *timer_ts, *timer_put;
 	int i, fifobuf[128], count = 0;
-	char s[11];
-	struct SHEET *sht_back;
+	char s[12];
 
 	fifo32_init(&fifo, 128, fifobuf);
 	timer_ts = timer_alloc();
-	timer_init(timer_ts, &fifo, 1);
+	timer_init(timer_ts, &fifo, 2);
 	timer_settime(timer_ts, 2);
-	sht_back = (struct SHEET *) *((int *) 0x0fec);
+	timer_put = timer_alloc();
+	timer_init(timer_put, &fifo, 1);
+	timer_settime(timer_put, 1);
 
 	for (;;) {
 		count++;
-		sprintf(s, "%10d", count);
-		putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
 		io_cli();
 		if (fifo32_status(&fifo) == 0) {
 			io_sti();
 		} else {
 			i = fifo32_get(&fifo);
 			io_sti();
-			if (i == 1) { /* タスクスイッチ */
+			if (i == 1) {
+				sprintf(s, "%11d", count);
+				putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 11);
+				timer_settime(timer_put, 1);
+			} else if (i == 2) {
 				farjmp(0, 3 * 8);
 				timer_settime(timer_ts, 2);
 			}
