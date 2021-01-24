1aca0df56014b73199fb5ba0485dbefde71aff0d
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-24 14:18:50 +0900

    up

--- src/console.c
+++ src/console.c
@@ -264,8 +264,9 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 		}
 		name[i] = cmdline[i];
 	}
-	name[i] = 0;
+	name[i] = 0; /* とりあえずファイル名の後ろを0にする */
 
+	/* ファイルを探す */
 	finfo = file_search(name, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
 	if (finfo == 0 && name[i - 1] != '.') {
 		/* 見つからなかったので後ろに".HRB"をつけてもう一度探してみる */
@@ -280,6 +281,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 	if (finfo != 0) {
 		/* ファイルが見つかった場合 */
 		p = (char *) memman_alloc_4k(memman, finfo->size);
+		*((int *) 0xfe8) = (int) p;
 		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));
 		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER);
 		farcall(0, 1003 * 8);
@@ -287,18 +289,20 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 		cons_newline(cons);
 		return 1;
 	}
+	/* ファイルが見つからなかった場合 */
 	return 0;
 }
 
 void hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
 {
+	int cs_base = *((int *) 0xfe8);
 	struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
 	if (edx == 1) {
 		cons_putchar(cons, eax & 0xff, 1);
 	} else if (edx == 2) {
-		cons_putstr0(cons, (char *) ebx);
+		cons_putstr0(cons, (char *) ebx + cs_base);
 	} else if (edx == 3) {
-		cons_putstr1(cons, (char *) ebx, ecx);
+		cons_putstr1(cons, (char *) ebx + cs_base, ecx);
 	}
 	return;
 }
