ef6391f5b363770dc889a9273be6585aa8de9273
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-30 14:34:54 +0900

    fix

--- src/hello4.c
+++ src/hello4.c
@@ -6,11 +6,3 @@ void HariMain(void)
 	api_putstr0("hello, world\n");
 	api_end();
 }
-void api_putstr0(char *s);
-void api_end(void);
-
-void HariMain(void)
-{
-	api_putstr0("hello, world\n");
-	api_end();
-}
