cbbf3f535c7367fe3e668d368c18bf114ee9e5b9
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-30 18:00:06 +0900

    up

--- src/console.c
+++ src/console.c
@@ -174,6 +174,7 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
 		cmd_type(cons, fat, cmdline);
 	} else if (cmdline[0] != 0) {
 		if (cmd_app(cons, fat, cmdline) == 0) {
+			/* コマンドではなく、アプリでもなく、さらに空行でもない */
 			cons_putstr0(cons, "Bad command.\n\n");
 		}
 	}
@@ -300,7 +301,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 			shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
 			for (i = 0; i < MAX_SHEETS; i++) {
 				sht = &(shtctl->sheets0[i]);
-				if (sht->flags != 0 && sht->task == task) {
+				if ((sht->flags & 0x11) == 0x11 && sht->task == task) {
 					/* アプリが開きっぱなしにした下じきを発見 */
 					sheet_free(sht);	/* 閉じる */
 				}
@@ -341,6 +342,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 	} else if (edx == 5) {
 		sht = sheet_alloc(shtctl);
 		sht->task = task;
+		sht->flags |= 0x10;
 		sheet_setbuf(sht, (char *) ebx + ds_base, esi, edi, eax);
 		make_window8((char *) ebx + ds_base, esi, edi, (char *) ecx + ds_base, 0);
 		sheet_slide(sht, 100, 50);
