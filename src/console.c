03984549cb4deec4e88274e8b400aa680d549c10
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-31 14:09:54 +0900

    up

--- src/console.c
+++ src/console.c
@@ -416,7 +416,19 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 				reg[7] = i - 256;
 				return 0;
 			}
+			if (i >= 256) { /* キーボードデータ（タスクA経由）など */
+				reg[7] = i - 256;
+				return 0;
+			}
 		}
+	} else if (edx == 16) {
+		reg[7] = (int) timer_alloc();
+	} else if (edx == 17) {
+		timer_init((struct TIMER *) ebx, &task->fifo, eax + 256);
+	} else if (edx == 18) {
+		timer_settime((struct TIMER *) ebx, eax);
+	} else if (edx == 19) {
+		timer_free((struct TIMER *) ebx);
 	}
 	return 0;
 }
