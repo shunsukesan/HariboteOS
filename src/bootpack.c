c23227c48b14b1ee2c879d24ea13e554fac928d8
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2021-01-31 15:57:58 +0900

    up

--- src/bootpack.c
+++ src/bootpack.c
@@ -17,9 +17,9 @@ void HariMain(void)
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-	unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons;
-	struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons;
-	struct TASK *task_a, *task_cons;
+	unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons[2];
+	struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons[2];
+	struct TASK *task_a, *task_cons[2];
 	struct TIMER *timer;
 	static char keytable0[0x80] = {
 		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
@@ -76,23 +76,27 @@ void HariMain(void)
 	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
 
 	/* sht_cons */
-	sht_cons = sheet_alloc(shtctl);
-	buf_cons = (unsigned char *) memman_alloc_4k(memman, 256 * 165);
-	sheet_setbuf(sht_cons, buf_cons, 256, 165, -1); /* 透明色なし */
-	make_window8(buf_cons, 256, 165, "console", 0);
-	make_textbox8(sht_cons, 8, 28, 240, 128, COL8_000000);
-	task_cons = task_alloc();
-	task_cons->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
-	task_cons->tss.eip = (int) &console_task;
-	task_cons->tss.es = 1 * 8;
-	task_cons->tss.cs = 2 * 8;
-	task_cons->tss.ss = 1 * 8;
-	task_cons->tss.ds = 1 * 8;
-	task_cons->tss.fs = 1 * 8;
-	task_cons->tss.gs = 1 * 8;
-	*((int *) (task_cons->tss.esp + 4)) = (int) sht_cons;
-	*((int *) (task_cons->tss.esp + 8)) = memtotal;
-	task_run(task_cons, 2, 2); /* level=2, priority=2 */
+	for (i = 0; i < 2; i++) {
+		sht_cons[i] = sheet_alloc(shtctl);
+		buf_cons[i] = (unsigned char *) memman_alloc_4k(memman, 256 * 165);
+		sheet_setbuf(sht_cons[i], buf_cons[i], 256, 165, -1); /* 透明色なし */
+		make_window8(buf_cons[i], 256, 165, "console", 0);
+		make_textbox8(sht_cons[i], 8, 28, 240, 128, COL8_000000);
+		task_cons[i] = task_alloc();
+		task_cons[i]->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
+		task_cons[i]->tss.eip = (int) &console_task;
+		task_cons[i]->tss.es = 1 * 8;
+		task_cons[i]->tss.cs = 2 * 8;
+		task_cons[i]->tss.ss = 1 * 8;
+		task_cons[i]->tss.ds = 1 * 8;
+		task_cons[i]->tss.fs = 1 * 8;
+		task_cons[i]->tss.gs = 1 * 8;
+		*((int *) (task_cons[i]->tss.esp + 4)) = (int) sht_cons[i];
+		*((int *) (task_cons[i]->tss.esp + 8)) = memtotal;
+		task_run(task_cons[i], 2, 2); /* level=2, priority=2 */
+		sht_cons[i]->task = task_cons[i];
+		sht_cons[i]->flags |= 0x20;	/* カーソルあり */
+	}
 
 	/* sht_win */
 	sht_win   = sheet_alloc(shtctl);
@@ -114,16 +118,16 @@ void HariMain(void)
 	my = (binfo->scrny - 28 - 16) / 2;
 
 	sheet_slide(sht_back,  0,  0);
-	sheet_slide(sht_cons, 32,  4);
+	sheet_slide(sht_cons[1], 56,  6);
+	sheet_slide(sht_cons[0],  8,  2);
 	sheet_slide(sht_win,  64, 56);
 	sheet_slide(sht_mouse, mx, my);
-	sheet_updown(sht_back,  0);
-	sheet_updown(sht_cons,  1);
-	sheet_updown(sht_win,   2);
-	sheet_updown(sht_mouse, 3);
+	sheet_updown(sht_back,     0);
+	sheet_updown(sht_cons[1],  1);
+	sheet_updown(sht_cons[0],  2);
+	sheet_updown(sht_win,      3);
+	sheet_updown(sht_mouse,    4);
 	key_win = sht_win;
-	sht_cons->task = task_cons;
-	sht_cons->flags |= 0x20;	/* カーソルあり */
 
 	/* 最初にキーボード状態との食い違いがないように、設定しておくことにする */
 	fifo32_put(&keycmd, KEYCMD_LED);
@@ -227,12 +231,12 @@ void HariMain(void)
 					fifo32_put(&keycmd, KEYCMD_LED);
 					fifo32_put(&keycmd, key_leds);
 				}
-				if (i == 256 + 0x3b && key_shift != 0 && task_cons->tss.ss0 != 0) {	/* Shift+F1 */
+				if (i == 256 + 0x3b && key_shift != 0 && task_cons[0]->tss.ss0 != 0) {	/* Shift+F1 */
 					cons = (struct CONSOLE *) *((int *) 0x0fec);
 					cons_putstr0(cons, "\nBreak(key) :\n");
 					io_cli();	/* 強制終了処理中にタスクが変わると困るから */
-					task_cons->tss.eax = (int) &(task_cons->tss.esp0);
-					task_cons->tss.eip = (int) asm_end_app;
+					task_cons[0]->tss.eax = (int) &(task_cons[0]->tss.esp0);
+					task_cons[0]->tss.eip = (int) asm_end_app;
 					io_sti();
 				}
 				if (i == 256 + 0x57 && shtctl->top > 2) {	/* F11 */
@@ -280,6 +284,11 @@ void HariMain(void)
 								if (0 <= x && x < sht->bxsize && 0 <= y && y < sht->bysize) {
 									if (sht->buf[y * sht->bxsize + x] != sht->col_inv) {
 										sheet_updown(sht, shtctl->top - 1);
+										if (sht != key_win) {
+											cursor_c = keywin_off(key_win, sht_win, cursor_c, cursor_x);
+											key_win = sht;
+											cursor_c = keywin_on(key_win, sht_win, cursor_c);
+										}
 										if (3 <= x && x < sht->bxsize - 3 && 3 <= y && y < 21) {
 											mmx = mx;	/* ウィンドウ移動モードへ */
 											mmy = my;
@@ -290,8 +299,8 @@ void HariMain(void)
 												cons = (struct CONSOLE *) *((int *) 0x0fec);
 												cons_putstr0(cons, "\nBreak(mouse) :\n");
 												io_cli();	/* 強制終了処理中にタスクが変わると困るから */
-												task_cons->tss.eax = (int) &(task_cons->tss.esp0);
-												task_cons->tss.eip = (int) asm_end_app;
+												task_cons[0]->tss.eax = (int) &(task_cons[0]->tss.esp0);
+												task_cons[0]->tss.eip = (int) asm_end_app;
 												io_sti();
 											}
 										}
